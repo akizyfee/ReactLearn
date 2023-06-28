d8d0e028e1f2507532c464ea5bcdacb13850dabb
Pause <87452790+akizyfee@users.noreply.github.com>
2023-06-28 14:15:33 +0800

    first commit

---
+++ src/apis/todo.ts
@@ -0,0 +1,3 @@
+import axios from '../plugins/axios'
+
+export const signIn = (payload:{}) => axios.post('/v2/admin/signin', payload)
---
+++ src/utils/cookie.ts
@@ -0,0 +1,13 @@
+import Cookies from 'js-cookie';
+
+export const clearCookieToken = () => {
+    Cookies.remove('test')
+}
+
+export const getCookieToken = () => {
+    return Cookies.get('test')
+}
+
+export const setCookieToken = (token:string) => {
+    Cookies.set('test', token)
+}
